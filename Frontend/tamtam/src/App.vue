55d6aa678b41227e2db8faf566f671bf0a8c8ec9
dohee479 <dohee479@naver.com>
2020-10-30 19:03:02 +0900

    S03P31B203-89 | A_nav-bar

--- Frontend/tamtam/src/App.vue
+++ Frontend/tamtam/src/App.vue
@@ -1,56 +1,30 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
+    <nav>
+      <span>TamTam</span>
+      <span>내 기업 분석</span>
+      <span>전체 기업 분석</span>
+      <span>SAMSUNG</span>
+      <span>
+        <span class="icon"><i class="fa fa-search"></i></span>
+        <input type="text" placeholder="검색 또는 동영상 URL을 입력해주세요." />
+      </span>
+    </nav>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
   components: {
-    HelloWorld
   },
-
   data: () => ({
     //
   })
 };
 </script>
+
+<style lang="scss">
+@import "./scss/reset.scss";
+@import "./scss/nav.scss";
+</style>
