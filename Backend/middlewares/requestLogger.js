fb85f7a1b509418888496f3ea83cb3d9f08706f1
이다인 <dlekdls0213@naver.com>
2020-11-03 17:23:21 +0900

    Change user.email

--- Backend/middlewares/requestLogger.js
+++ Backend/middlewares/requestLogger.js
@@ -3,9 +3,7 @@ const requestLogger = (request, response, next) => {
   const start = new Date().getTime()
   response.on('finish', () => {
     const elapsed = new Date().getTime() - start
-    console.info(
-      `${request.method} ${request.originalUrl} ${response.statusCode} ${elapsed}ms`
-    )
+    console.info(`${request.method} ${request.originalUrl} ${response.statusCode} ${elapsed}ms`)
   })
   next()
 }
