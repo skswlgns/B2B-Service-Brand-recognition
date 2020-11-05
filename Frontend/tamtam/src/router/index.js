a674a4b947208dde0113e2ecc0efd24eeb8d1615
김용욱 <emplam27@gmail.com>
2020-11-05 13:55:02 +0900

    S03P31B203-120 | C Video Detail

--- Frontend/tamtam/src/router/index.js
+++ Frontend/tamtam/src/router/index.js
@@ -9,6 +9,7 @@ import SearchVideo from '@/components/SearchVideo.vue'
 import SearchChannel from '@/components/SearchChannel.vue'
 import Login from '../views/Login.vue'
 import Main from '@/views/Main.vue'
+import VideoDetail from '@/views/VideoDetail.vue'
 
 Vue.use(VueRouter)
 
@@ -63,6 +64,13 @@ const routes = [
     path: '/',
     name: 'Main',
     component: Main
+  },
+
+  // 용욱
+  {
+    path: '/video/:video_youtube_id',
+    name: 'VideoDetail',
+    component: VideoDetail
   }
 ]
 
