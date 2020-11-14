b352ff7d625ad9d5d581a76acc184f94ca9362a8
dohee479 <dohee479@naver.com>
2020-11-14 16:56:19 +0900

    S03P31B203-152 | U_Rank

--- Backend/routes/searchRoutes.js
+++ Backend/routes/searchRoutes.js
@@ -25,8 +25,24 @@ searchRoutes.get('/subscribe', async (req, res) => {
   }
 })
 
+// 구독자 전체
+searchRoutes.get('/subscribes', async (req, res) => {
+  if (req.headers.token) {
+    const limit = parseInt(req.headers.limit)
+    try {
+      let channelAll = await ChannelModel.find().sort({ channel_subscribe: -1 })
+      channelAll = channelAll.slice(limit, limit + 10)
+      res.status(200).send(channelAll)
+    } catch (err) {
+      res.status(500).send(err)
+    }
+  } else {
+    res.status(403).send({ message: '로그인이 필요한 서비스입니다.' })
+  }
+})
+
 // 구독자 대비 평균 조회수 TOP 5
-searchRoutes.get('/avgviews', async (req, res) => {
+searchRoutes.get('/avgview', async (req, res) => {
   if (req.headers.token) {
     try {
       const channelAll = await ChannelModel.find()
@@ -56,6 +72,43 @@ searchRoutes.get('/avgviews', async (req, res) => {
     res.status(403).send({ message: '로그인이 필요한 서비스입니다.' })
   }
 })
+
+// 구독자 대비 평균 조회수
+searchRoutes.get('/avgviews', async (req, res) => {
+  if (req.headers.token) {
+    const limit = parseInt(req.headers.limit)
+    try {
+      const channelAll = await ChannelModel.find()
+
+      for (let i = 0; i < channelAll.length; i++) {
+        const videos = await VideoModel.find({ channel_id: channelAll[i]._id })
+
+        let totalview = 0
+        for (let j = 0; j < videos.length; j++) {
+          totalview += videos[j].video_views
+        }
+        let result = 0
+        if (channelAll[i].channel_subscribe === 0) {
+          result = 0
+        } else {
+          result = totalview / channelAll[i].channel_subscribe
+          result = result.toFixed(3)
+        }
+        await ChannelModel.findOneAndUpdate({ _id: channelAll[i]._id }, { channel_avg_views: result })
+      }
+      let youtubers = await ChannelModel.find().sort({ channel_avg_views: -1 })
+      youtubers = youtubers.slice(limit, limit + 10)
+      res.status(200).send(youtubers)
+    } catch (err) {
+      res.status(500).send(err)
+    }
+  } else {
+    res.status(403).send({ message: '로그인이 필요한 서비스입니다.' })
+  }
+})
+
+// 비디오 조회수
+searchRoutes.get('/')
 
 // 추천영상
 searchRoutes.get('/recommend', async (req, res) => {
