300dca3f3df0f7e61fdd2dc779e4c8b2712b8cbe
김승범 <tmdwkd2@naver.com>
2020-11-16 03:13:22 +0900

    S03P31B203-165 | U css

--- Frontend/tamtam/src/components/CompanyAnalysisComponents/RecommendVideo.vue
+++ Frontend/tamtam/src/components/CompanyAnalysisComponents/RecommendVideo.vue
@@ -163,7 +163,7 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 2;
+  -webkit-line-clamp: 1;
   -webkit-box-orient: vertical;
   word-wrap: break-word;
   margin-top: 8px;
